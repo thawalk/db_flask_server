9ea396904973ec143a8e15226ce903305630905f
akmalhakimteo <akmal_hakim_teo@hotmail.com>
2020-11-22 19:18:35 +0800

    mongoDB not returning anything issue fixed

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
 import json
 from flask import Flask, jsonify, url_for, request, redirect,Response,Request
 # from flask_pymongo import PyMongo
-from pymongo import MongoClient
+import pymongo
 from bson.json_util import dumps
 import mysql.connector
 from werkzeug.serving import run_simple
 import os
 from dotenv import load_dotenv
 import datetime
+import time
 
 app = Flask(__name__)
-
 # mongo_url = os.getenv("mongo_url")
 # dbname = os.getenv("database_name")
 # mongo_store = MongoClient(mongo_url)
 # metadata = mongo_store.dbname.metadata
 
-bruh='test_collection'
+test_collection='test_collection'
 # sample='user_collection'
-mongo_url ="54.211.223.244"
-dbname ='test'
-mongo_store = MongoClient(mongo_url)
-metadata = mongo_store.dbname.bruh
+mongo = pymongo.MongoClient('mongodb://54.211.223.244:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+db = pymongo.database.Database(mongo, 'test')
+metadata_col = pymongo.collection.Collection(db, 'test_collection')
+
+
+
+# impt = dumps(list(col.find({"asin":"1603420304"})))
+# print(impt)
+
+# list_data = list(data)
+# data_print = dumps(list_data)
+# print(data_print)
 # data = mongo_store.dbname.sample
-print(metadata.find({'price': '19.99'}))
+# print("testing metadata find")
+# print(dumps(list(metadata.find().limit(10))))
+
+
+
+
 
 db = mysql.connector.connect(
     host ='52.87.158.130',
@@ -34,13 +47,14 @@ db = mysql.connector.connect(
 
 
 cur = db.cursor()
-cur.execute("SELECT asin from kindle_reviews group by asin order by avg(overall) desc limit 9 ")
-print(cur.fetchall())
+# cur.execute("SELECT asin from kindle_reviews group by asin order by avg(overall) desc limit 9 ")
+# print(cur.fetchall())
+# print("above fetch all")
 
 @app.route('/',methods=["GET"])
 def api_root():
     data = {
-        'message': 'Welcome to SUTD room service. /rooms to see more info'
+        'message': 'Welcome to our website. Where reviews are our number one priority'
     }
     js = json.dumps(data)
     response = Response(js, status=200, mimetype='application/json')
@@ -50,22 +64,31 @@ def api_root():
 @app.route('/categories', methods = ['GET'])
 def get_categories():
     categories = []
+    js = json.dumps(data)
+    response = Response(js, status=200, mimetype='application/json')
+    return response
 
 #Search for book using title, price or asin
-@app.route('/search', methods=['GET'])
+@app.route('/search', methods=['GET'])  #lets fix this
 def search_book():
-    book = []
-    if title in request.args:
-        book = metadata.find({'title': title})
-    elif price in request.args:
-        book = metadata.find({'price':price})
-    elif asin in request.args:
-        book = metadata.find({'asin':asin})
-    if len(book) > 0:
-        msg = {'status': 200, 'message': 'book(s) successfully found', 'books': book}
-    else :
-        msg = {'status': 500, 'message': 'no books found with the following searches'}
-    return jsonify(msg)
+    # print("search endpoint")
+    data = dumps(list(metadata_col.find().limit(10)))
+    print(data)
+    js = json.dumps(data)
+    response = Response(js, status=200, mimetype='application/json')
+    return response
+    # book = []
+    # if title in request.args:
+    #     book = metadata.find({'title': title})
+    # elif price in request.args:
+    #     book = metadata.find({'price':price})
+    # elif asin in request.args:
+    #     book = metadata.find({'asin':asin})
+    # if len(book) > 0:
+    #     msg = {'status': 200, 'message': 'book(s) successfully found', 'books': book}
+    # else :
+    #     msg = {'status': 500, 'message': 'no books found with the following searches'}
+    # return jsonify(msg)
 
 @app.route('/review', methods=['POST'])
 def add_review():
@@ -82,4 +105,5 @@ def add_review():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    # app.run(host="0.0.0.0", port=80)
+    app.run(debug=True)
